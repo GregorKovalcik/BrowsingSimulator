4e739fb5e1947570d502bfa7bc8b7be3212076fc
Gregor Kovalcik <gregor.kovalcik@gmail.com>
2017-07-27 14:18:54 +0200

    Added class mapping generator.

---
+++ BrowsingSimulator/GenerateClassMappingFile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateClassMappingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDirectory = args[0];
+            string outputFile = args[1];
+            
+            string[] subdirectories = Directory.GetDirectories(inputDirectory);
+
+            int classId = 0;
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                for (int i = 0; i < subdirectories.Length; i++)
+                {
+                    string subDirectory = subdirectories[i];
+
+                    // check if directory
+                    FileAttributes attr = File.GetAttributes(subDirectory);
+                    if ((attr & FileAttributes.Directory) != FileAttributes.Directory)
+                    {
+                        throw new ArgumentException("Path is not a directory: " + subDirectory);
+                    }
+
+                    // count subdir files
+                    int fileCount = Directory.GetFiles(subDirectory).Length;
+
+                    // write class mapping for each file
+                    for (int f = 0; f < fileCount; f++)
+                    {
+                        writer.WriteLine(classId);
+                    }
+
+                    // increment variable
+                    classId++;
+                }
+            }
+        }
+    }
+}
